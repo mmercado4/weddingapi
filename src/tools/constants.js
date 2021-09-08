5af82ede2ff57791367942e866e17266a760c8d6
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-09-08 15:42:46 +0200

    fix(api): allow all uris in cors

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -2,7 +2,7 @@ const APIPORT = 3333;
 const HOST = "localhost";
 const FULLHOST = "http://localhost:";
 const WEB_DEV_PORT = 3434;
-const WEB_URL = "https://marinaycarlos.herokuapp.com/";
+const WEB_URL = "*";
 // process.env.NODE_ENV === "production"
 //   ? "https://marinaycarlos.herokuapp.com/"
 //   : `${FULLHOST}${WEB_DEV_PORT}`;
