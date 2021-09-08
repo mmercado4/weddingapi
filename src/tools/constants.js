e079ae899ec7fd7022f095a25441d96620bea03a
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-09-08 15:30:15 +0200

    fix(api): change web url for cors

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -2,14 +2,15 @@ const APIPORT = 3333;
 const HOST = "localhost";
 const FULLHOST = "http://localhost:";
 const WEB_DEV_PORT = 3434;
-const WEB_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://marinaycarlos.herokuapp.com/"
-    : `${FULLHOST}${WEB_DEV_PORT}`;
+const WEB_URL = "https://marinaycarlos.herokuapp.com/"(
+  // process.env.NODE_ENV === "production"
+  //   ? "https://marinaycarlos.herokuapp.com/"
+  //   : `${FULLHOST}${WEB_DEV_PORT}`;
 
-module.exports = {
-  APIPORT: APIPORT,
-  HOST: HOST,
-  FULLHOST: FULLHOST,
-  WEB_URL: WEB_URL,
-};
+  (module.exports = {
+    APIPORT: APIPORT,
+    HOST: HOST,
+    FULLHOST: FULLHOST,
+    WEB_URL: WEB_URL,
+  })
+);
