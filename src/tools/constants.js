fe93f2ab18eb1945d8240d51ee2116800f947712
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-10-25 22:03:31 +0200

    feat(api): token changes

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -3,9 +3,9 @@ const HOST = "localhost";
 const FULLHOST = "http://localhost:";
 const WEB_DEV_PORT = 3434;
 //const WEB_URL = "http://marinaycarlos.es";
-const WEB_URL = "https://marinaycarlos.herokuapp.com";
+//const WEB_URL = "https://marinaycarlos.herokuapp.com";
 
-//const WEB_URL = "*";
+const WEB_URL = "*";
 
 module.exports = {
   APIPORT: APIPORT,
