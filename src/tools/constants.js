5f71eb32fa618e80b0f5de75bd8c6b50600ddeac
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-10-27 18:53:25 +0200

    feat(api): change final https uri cors

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -2,7 +2,8 @@ const APIPORT = 3333;
 const HOST = "localhost";
 const FULLHOST = "http://localhost:";
 const WEB_DEV_PORT = 3434;
-const WEB_URL = "http://www.marinaycarlos.es";
+const WEB_URL = "https://www.marinaycarlos.es";
+//const WEB_URL = "http://www.marinaycarlos.es";
 //const WEB_URL = "https://marinaycarlos.herokuapp.com";
 
 //const WEB_URL = "*";
